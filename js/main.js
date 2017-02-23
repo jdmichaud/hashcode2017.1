0fca733a4826023644934b924110da66fab70c83
jd <jean.daniel.michaud@gmail.com>
2017-02-23 20:11:35 +0100

    Integrate scoring.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 const hashcode = require('./hashcode');
 const optimize = require('./optimize');
+const scoreModule = require('./score');
 
 console.log('--------- BEGIN -----------');
 const setup = hashcode.loadFile(process.argv[2]);
 console.log('setup:', setup);
 const result = optimize.optimize(setup);
 console.log('result:', JSON.stringify(result, null, 4));
+const score = scoreModule.getScore(setup, result);
+console.log('score:', score);
 console.log('---------- END ------------');
 // const results = hashcode.streamResult(bestSlices);
 // hashcode.saveFile(results, `${process.argv[2].slice(0, -2)}out`);
