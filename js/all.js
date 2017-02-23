22be33751b5565d17a483cc9e8c4f60b6272ce35
Josselin Buils <josselin.buils@gmail.com>
2017-02-23 20:54:06 +0100

    Generate folders with output files

--- js/all.js
+++ js/all.js
@@ -6,39 +6,47 @@ const optimize = require('./optimize');
 const scoreModule = require('./score');
 
 const inputDir = '../input';
+const outputDir = '../output';
 
 function apply(fileName) {
-   let time = Date.now();
+    let time = Date.now();
 
     console.log('\n--------- BEGIN -----------');
-    console.log('file:', fileName)
+    console.log('file:', fileName);
     const setup = hashcode.loadFile(path.join(inputDir, fileName));
-    //console.log('setup:', setup);
     const result = optimize.optimize(setup);
-    //console.log('result:', JSON.stringify(result, null, 4));
     const score = scoreModule.getScore(setup, result);
     console.log('time:', (Date.now() - time) + 'ms');
     console.log('score:', score);
     console.log('---------- END ------------');
-    hashcode.saveFile(hashcode.streamResult(result), `${fileName.slice(0, -2)}out`);
-    // const results = hashcode.streamResult(bestSlices);
-    // hashcode.saveFile(results, `${process.argv[2].slice(0, -2)}out`);
-    // console.log('--------- BEGIN -----------');
-    // console.log(results);
-    // console.log('---------- END ------------');
 
-    return score;
+    return {
+        result: result,
+        score: score
+    };
 }
 
 function applyAll() {
     let fileNames = fs.readdirSync(inputDir).filter(fileName => /^[a-z_]+\.in$/.test(fileName)),
+        outputs = [],
         totalScore = 0;
 
     fileNames.forEach(fileName => {
-        totalScore += apply(fileName);
+        var res = apply(fileName);
+        outputs.push({
+            fileName: fileName,
+            result: res.result
+        });
+        totalScore += res.score;
     });
 
     console.log('\nTotal score:', totalScore, '\n');
+
+    let outputFolder = path.join(outputDir, Date.now() + ' ' + totalScore);
+
+    fs.mkdirSync(outputFolder);
+
+    outputs.forEach(output => hashcode.saveFile(hashcode.streamResult(output.result), path.join(outputFolder, output.fileName.slice(0, -2) + 'out')));
 }
 
 applyAll();
